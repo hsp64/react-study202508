d46aceab592592c065a7bbf12fbb12f9abada66a
hsp64 <pphs604@gmail.com>
2025-08-27 12:38:32 +0900

    실습문제 2-1-1 체크리스트 완성

--- src/components/Todos/TodoItem.jsx
+++ src/components/Todos/TodoItem.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { MdDelete, MdDone } from 'react-icons/md';
 
 import styles from './scss/TodoItem.module.scss';
 
 const TodoItem = () => {
 
-    const { text, remove, 'todo-list-item': itemStyle, 'check-circle': checkCircle } = styles;
+    const {
+        text,
+        remove,
+        'todo-list-item': itemStyle,
+        'check-circle': checkCircle,
+        active,
+        finish
+    } = styles;
+
+    const [isActive, setIsActive] = useState(false);
+
+    const changeHandler = e => {
+        setIsActive(!isActive);
+    };
 
     return (
         <li className={itemStyle}>
-            <div className={checkCircle}>
-                <MdDone />
+            <div className={`${checkCircle} ${isActive ? active : ''}`} onClick={changeHandler}>
+                {isActive && <MdDone />}
             </div>
-            <span className={text}>할 일 어쩌구~~</span>
+            <span className={`${text} ${isActive ? finish : ''}`}>할 일 어쩌구~~</span>
             <div className={remove}>
                 <MdDelete />
             </div>
