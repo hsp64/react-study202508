603a2ccfe519d6b82e25c32b51bd020d410f9769
hsp64 <pphs604@gmail.com>
2025-08-28 14:21:39 +0900

    6장 복습예제. 4- 타이머 챌린지 기본 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Player from './components/TimerGame/Player.jsx';
+import TimerChallenge from "./components/TimerGame/TimerChallenge.jsx";
 
 const App = () => {
+
+
 
     return (
         <>
             <Player />
+            <div id="Challenges">
+                <TimerChallenge title='Easy' targetTime={1} />
+                <TimerChallenge title='Not Easy' targetTime={5}/>
+                <TimerChallenge title='Getting tough' targetTime={10}/>
+                <TimerChallenge title='Pros only' targetTime={15}/>
+            </div>
         </>
     );
 };
---
+++ src/components/TimerGame/TimerChallenge.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+const TimerChallenge = ({ title, targetTime }) => {
+
+    // 타이머가 시작됐는지를 확인하는 상태값
+    const [timerStarted, setTimerStarted] = useState(false);
+
+    // 타이머가 만료되었는지 확인하는 상태값
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    const handleStart = e => {
+        setTimerStarted(true);
+
+        // 실제 정해진 시간만큼 타이머를 가동
+        setTimeout(() => {
+            console.log(targetTime + 's 타이머 만료!');
+            setTimerExpired(true);
+        }, targetTime * 1000);
+    };
+
+    return (
+        <section className='challenge'>
+            <h2>{title}</h2>
+            {timerExpired && <p>You Lost!</p>}
+            <p className='challenge-time'>
+                {targetTime} second{targetTime > 1 ? 's' : ''}
+            </p>
+            <p>
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
+                </button>
+            </p>
+            <p>
+                {timerStarted ? 'Time is running...' : 'Timer inactive'}
+            </p>
+        </section>
+    );
+};
+
+export default TimerChallenge;
