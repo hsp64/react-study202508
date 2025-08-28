e167678b4e019a842c60a86236237546620609d9
hsp64 <pphs604@gmail.com>
2025-08-28 10:43:56 +0900

    5-2-6. React Portal 리팩토링

--- src/components/ui/Modal/ErrorModal.jsx
+++ src/components/ui/Modal/ErrorModal.jsx
@@ -3,21 +3,40 @@ import Card from '../Card';
 import Button from '../Button';
 import styles from './ErrorModal.module.css';
 
+// portal을 사용하기 위한 import
+import ReactDOM from 'react-dom';
+import Portal from './Portal.jsx';
+
+const BackDrop = ({ onClose }) => {
+    return <div className={styles.backdrop} onClick={onClose}></div>
+};
+
+const ModalOverlay = ({title, message, onClose}) => {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{title}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{message}</p>
+            </div>
+            <footer className={styles.actions}>
+                <Button onClick={onClose}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
+
 const ErrorModal = ({ title, message, onClose }) => {
     return (
         <>
-            <div className={styles.backdrop} onClick={onClose}></div>
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{title}</h2>
-                </header>
-                <div className={styles.content}>
-                    <p>{message}</p>
-                </div>
-                <footer className={styles.actions}>
-                    <Button onClick={onClose}>Okay</Button>
-                </footer>
-            </Card>
+            <Portal destId='backdrop-root'>
+                <BackDrop onClose={onClose} />
+            </Portal>
+
+            <Portal destId='modal-overlay-root'>
+                <ModalOverlay title={title} message={message} onClose={onClose} />
+            </Portal>
         </>
     );
 };
